5ffc36d6d5aa2817f90f6ba0ffb979d33503b880
BrenoBar51 <brenobarcelos9@gmail.com>
2023-09-20 09:45:14 -0300

    ok

---
+++ aula04/index.js
@@ -0,0 +1,33 @@
+//digitalcollege-classes/fs17
+const http = require('http');
+const db = require('./conaxao');
+const db2 = require('./conexaolabs');
+
+async function recepcao(req, res){
+    if(req.url === '/alunos'){
+        let dados = await db.executar('SELECT * FROM tb_aluno'); //SELECT * FROM tb_aluno;
+        res.end(JSON.stringify(dados));
+        return;
+    }
+
+    if(req.url === '/labs'){
+        let dados = await db.executar('SELECT * FROM tb_lab');
+        res.end(JSON.stringify(dados));
+        return;
+    }
+
+    if(req.url === '/professores'){
+        let dados = await db.executar('SELECT * FROM tb_lab');
+        res.end(JSON.stringify(dados));
+        return;
+    }
+
+    res.end('API ta ON');
+}
+
+http.createServer(recepcao).listen(8000);
+
+
+//http.createServer((req, res) => {
+//    res.end('API ta ON')
+//}).listen(8000);
