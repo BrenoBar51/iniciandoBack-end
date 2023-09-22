b7994fc8ab18e2d378fa2b3312a2976f8664b3ef
BrenoBar51 <brenobarcelos9@gmail.com>
2023-09-21 22:48:52 -0300

    ok

--- aula03sql/db.sql
+++ aula03sql/db.sql
@@ -33,47 +33,39 @@ ALTER TABLE tb_professor ADD COLUMN formacao VARCHAR(20);
 --Inserir dados na tabela--
 --lab--
 INSERT INTO tb_lab (nome, cor, capacidade)
-VALUE ('Mark', 'Verde', '32');
+VALUE ('Mark', 'Verde', '10');
 INSERT INTO tb_lab (nome, cor, capacidade)
 VALUE ('Larry', 'Azul', '32');
 INSERT INTO tb_lab (nome, cor, capacidade)
-VALUE ('Jack', 'Roxa', '32');
+VALUE ('Jack', 'Roxa', '20');
 INSERT INTO tb_lab (nome, cor, capacidade)
-VALUE ('Elon Musk', 'Vermelha', '32');
+VALUE ('Elon Musk', 'Vermelha', '18');
 INSERT INTO tb_lab (nome, cor, capacidade)
-VALUE ('Bill Gates', 'Cinza', '32');
+VALUE ('Bill Gates', 'Cinza', '28');
 INSERT INTO tb_lab (nome, cor, capacidade)
-VALUE ('Steve Jobs', 'Cinza', '32');
+VALUE ('Steve Jobs', 'Cinza', '15');
 --Alunos--
 INSERT INTO tb_aluno (nome, email, telefone, data_nascimento)
-VALUE ('Geraldin Zica Du Pantano', 'GzinduPantano@gmail.com', '8588988980', '1999-02-03');
+VALUE 
+ ('Geraldin Zica Du Pantano', 'GzinduPantano@gmail.com', '8588988980', '1999-02-03'),
+ ('Manoel Gomes', 'manelgomi@gmail.com', '117787799', '1970-02-03'),
+ ('Kid Bengala', 'bengaladelicious@gmail.com', '88988888889', '1960-02-03'),
+ ('Tigresa VIP', 'AIDS2atualizado@gmail.com', '9288977980', '1980-02-03'),
+ ('Jailsson Mendes', 'JaJAOCO@gmail.com', '1688944980', '1987-02-03'),
+ ('Faustão Gordo', 'Tamanhoextraggggg@gmail.com', '8887654321', '1977-02-03'),
+ ('Talyson Loucuras', 'Thales@gaymail.com', '8588988981', '2002-02-03'),
+ ('Geraldo Luis', 'GLuau@gmail.com', '8588989980', '2005-02-03'),
+ ('Zeca Palgordinho', 'Zgordin@gmail.com', '8566988980', '1899-02-03'),
+ ('Peter Aqui', 'Picaqui@gmail.com', '8588989980', '1000-01-01');
 
-INSERT INTO tb_aluno (nome, email, telefone, data_nascimento)
-VALUE ('Manoel Gomes', 'manelgomi@gmail.com', '117787799', '1970-02-03');
-
-INSERT INTO tb_aluno (nome, email, telefone, data_nascimento)
-VALUE ('Kid Bengala', 'bengaladelicious@gmail.com', '88988888889', '1960-02-03');
-
-INSERT INTO tb_aluno (nome, email, telefone, data_nascimento)
-VALUE ('Tigresa VIP', 'AIDS2atualizado@gmail.com', '9288977980', '1980-02-03');
-
-INSERT INTO tb_aluno (nome, email, telefone, data_nascimento)
-VALUE ('Jailsson Mendes', 'JaJAOCO@gmail.com', '1688944980', '1987-02-03');
-
-INSERT INTO tb_aluno (nome, email, telefone, data_nascimento)
-VALUE ('Faustão Gordo', 'Tamanhoextraggggg@gmail.com', '8887654321', '1977-02-03');
-
-INSERT INTO tb_aluno (nome, email, telefone, data_nascimento)
-VALUE ('Talyson Loucuras', 'Thales@gaymail.com', '8588988981', '2002-02-03');
-
-INSERT INTO tb_aluno (nome, email, telefone, data_nascimento)
-VALUE ('Geraldo Luis', 'GLuau@gmail.com', '8588989980', '2005-02-03');
-
-INSERT INTO tb_aluno (nome, email, telefone, data_nascimento)
-VALUE ('Zeca Palgordinho', 'Zgordin@gmail.com', '8566988980', '1899-02-03');
-
-INSERT INTO tb_aluno (nome, email, telefone, data_nascimento)
-VALUE ('Peter Aqui', 'Picaqui@gmail.com', '8588989980', '1000-01-01');
+ --Professores--
+ INSERT INTO tb_professor (novato, nome, email, telefone, data_nascimento, linkedln )
+    VALUE 
+        ('2', 'Ale Capoeira', 'phpinga@gmail.com', '85988221133', '1985-04-08', 'https://www.linkedin.com/in/alephpvive/'),
+        ('1', 'Iberê Tenorio', 'manualdomundo@gmail.com', '85988441133', '1988-09-06', 'https://www.linkedin.com/in/ibererafael/'),
+        ('2', 'Nando Moura', 'meprocessaaivai@gmail.com', '85988221199', '1987-05-22', 'https://www.linkedin.com/in/mestredosprocessos/'),
+        ('2', 'Alexandre Frota', 'comiodecima@gmail.com', '11988551133', '1979-10-29', 'https://www.linkedin.com/in/frotachangeteam/'),
+        ('1', 'Mango Joe', 'aizedamanga@gmail.com', '85988773344', '2000-05-07', 'https://www.linkedin.com/in/mangojoe/');
 --Buscar "todos os dados de todos os labs"--
 SELECT * FROM tb_lab;
 
@@ -91,3 +83,12 @@ SELECT * FROM tb_lab WHERE capacidade=20;
 UPDATE tb_lab SET nome = 'Steve' WHERE id=2;
 
 DELETE FROM tb_aluno WHERE id=3;
+--gambiarras por preguiça--
+UPDATE tb_aluno SET id = 3 WHERE id=4;
+UPDATE tb_aluno SET id = 4 WHERE id=5;
+UPDATE tb_aluno SET id = 5 WHERE id=6;
+UPDATE tb_aluno SET id = 6 WHERE id=7;
+UPDATE tb_aluno SET id = 7 WHERE id=8;
+UPDATE tb_aluno SET id = 8 WHERE id=9;
+UPDATE tb_aluno SET id = 9 WHERE id=10;
+UPDATE tb_aluno SET id = 10 WHERE id=11;
