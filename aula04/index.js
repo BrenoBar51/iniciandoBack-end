0155402d513b6a4c9408980cd1fd3bc61530be63
BrenoBar51 <brenobarcelos9@gmail.com>
2023-09-21 21:58:04 -0300

    ok

--- aula04/index.js
+++ aula04/index.js
@@ -1,32 +1,30 @@
 //digitalcollege-classes/fs17
-const http = require('http');
-const db = require('./conaxao');
-const db2 = require('./conexaolabs');
+const http = require("http");
+const db = require("./conaxao");
 
-async function recepcao(req, res){
-    if(req.url === '/alunos'){
-        let dados = await db.executar('SELECT * FROM tb_aluno'); //SELECT * FROM tb_aluno;
-        res.end(JSON.stringify(dados));
-        return;
-    }
+async function recepcao(req, res) {
+  if (req.url === "/alunos") {
+    let dados = await db.executar("SELECT * FROM tb_aluno"); //SELECT * FROM tb_aluno;
+    res.end(JSON.stringify(dados));
+    return;
+  }
 
-    if(req.url === '/labs'){
-        let dados = await db.executar('SELECT * FROM tb_lab');
-        res.end(JSON.stringify(dados));
-        return;
-    }
+  if (req.url === "/labs") {
+    let dados = await db.executar("SELECT * FROM tb_lab");
+    res.end(JSON.stringify(dados));
+    return;
+  }
 
-    if(req.url === '/professores'){
-        let dados = await db.executar('SELECT * FROM tb_professor');
-        res.end(JSON.stringify(dados));
-        return;
-    }
+  if (req.url === "/professores") {
+    let dados = await db.executar("SELECT * FROM tb_professor");
+    res.end(JSON.stringify(dados));
+    return;
+  }
 
-    res.end('API ta ON');
+  res.end("API ta ON");
 }
 
 http.createServer(recepcao).listen(8000);
-
 
 //http.createServer((req, res) => {
 //    res.end('API ta ON')
